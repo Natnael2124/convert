1ecdaf913a70e5be520fd889547af5f6a027d442
Natnael2124 <natnaelbajo24@gmail.com>
2025-07-22 18:55:18 +0300

    add  a test for float that approximate the expectation

--- test_convert.py
+++ test_convert.py
@@ -1,7 +1,14 @@
+import pytest
 from convert import convert
 
-def test_conversion():
+def test_int_conversion():
     assert convert(1) == 149597870700
     assert convert(0) == 0
     assert convert(2.5) == 2.5 * 149597870700
 
+def test_error():
+    with pytest.raises(TypeError):
+        convert("1")
+
+def test_float_conversion():
+    assert convert(0.001) == pytest.approx(149597870.691, abs=1e-2)
